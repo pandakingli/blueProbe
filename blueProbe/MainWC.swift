27a4f78e72b0d3aaab2b8b7efa16b1ec1c06d7ee
mac2015 <utopia.pro@aliyun.com>
2018-03-16 11:15:26 +0800

    fix

--- blueProbe/MainWC.swift
+++ blueProbe/MainWC.swift
@@ -159,7 +159,7 @@ class MainWC: NSWindowController {
         BPSettingCenter.sharedInstance.keyClassName = self.superSelect.selectedItem!.title
 
         gMaker.goDoBNode()
-//
+
     }
     
     func startRunningAni()
