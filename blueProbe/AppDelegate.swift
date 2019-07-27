a634dd9aeee5f14498989147db864b786e6694ff
gabrielxpanda <gabrielxpanda@gmail.com>
2019-07-28 01:06:21 +0800

    fix

--- blueProbe/AppDelegate.swift
+++ blueProbe/AppDelegate.swift
@@ -13,10 +13,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
    // @IBOutlet weak var window: NSWindow!
     var mainWindowC :MainWC = MainWC()
+    var myWindow : mainWindow = mainWindow()
     
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-       
+        /*
+         //纯代码实现
+        myWindow = mainWindow.init(contentRect: NSMakeRect(0, 0, 400, 300), styleMask:[.closable,.resizable,.titled,.miniaturizable,.resizable], backing: NSWindow.BackingStoreType.buffered, defer: true)
+        myWindow.title = "测试"
+        myWindow.backgroundColor = NSColor.white
+        myWindow.makeKeyAndOrderFront(self)
+        myWindow.center()
+        */
         mainWindowC = MainWC.init(windowNibName: NSNib.Name(rawValue: "MainWC"))
         mainWindowC.window?.center()//让显示的位置居于屏幕的中心
         mainWindowC.window?.orderFront(nil)   //前置显示窗口
