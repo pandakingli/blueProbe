12842bb78584a447b324df0c364fb421ff6177ab
gabrielxpanda <gabrielxpanda@gmail.com>
2019-07-28 01:04:59 +0800

    fix

---
+++ blueProbe/mainWindow.swift
@@ -0,0 +1,14 @@
+//
+//  mainWindow.swift
+//  blueProbe
+//
+//  Created by Gabriel Fire Panda on 2019/7/28.
+//  Copyright © 2019 biubiublue. All rights reserved.
+//
+
+import Cocoa
+
+class mainWindow: NSWindow {
+   
+    
+}
